d6e8fac0d4b284ab52701c0ff5c7615c45da03b1
Lake Yin <la11010e@gmail.com>
2018-06-28 14:02:51 -0400

    Add JavaDoc for TankDrive

--- ARCCore/src/main/com/andoverrobotics/core/drivetrain/TankDrive.java
+++ ARCCore/src/main/com/andoverrobotics/core/drivetrain/TankDrive.java
@@ -19,6 +19,16 @@ public class TankDrive extends DriveTrain {
   private final int ticksPer360;
 
   // Verify that the motor(s) on one side is reversed if the motors point in opposite directions!
+
+  /**
+   * Creates a TankDrive from two IMotors
+   *
+   * @param motorL The left IMotor
+   * @param motorR The right IMotor
+   * @param opMode The OpMode to set
+   * @param ticksPerInch How many ticks per inch of displacement
+   * @param ticksPer360 How many ticks in one full rotation of the motor
+   */
   public TankDrive(IMotor motorL, IMotor motorR, OpMode opMode,
       int ticksPerInch, int ticksPer360) {
     super(opMode);
@@ -29,22 +39,51 @@ public class TankDrive extends DriveTrain {
     this.ticksPer360 = ticksPer360;
   }
 
-  public static TankDrive fromMotors(DcMotor left, DcMotor right, OpMode opMode,
+  /**
+   * Creates a TankDrive from two DcMotors.
+   *
+   * @param motorL The left DcMotor
+   * @param motorR The right DcMotor
+   * @param opMode The OpMode to set
+   * @param ticksPerInch How many ticks per inch of displacement
+   * @param ticksPer360 How many ticks in one full rotation of the motor
+   *
+   * @return A TankDrive created with the inputted motors
+   */
+  public static TankDrive fromMotors(DcMotor motorL, DcMotor motorR, OpMode opMode,
       int ticksPerInch, int ticksPer360) {
 
-    return new TankDrive(new MotorAdapter(left),
-        new MotorAdapter(right), opMode, ticksPerInch, ticksPer360);
+    return new TankDrive(new MotorAdapter(motorL),
+        new MotorAdapter(motorR), opMode, ticksPerInch, ticksPer360);
   }
 
-  public static TankDrive fromMotors(DcMotor left1, DcMotor left2, DcMotor right1, DcMotor right2,
+  /**
+   * Creates a TankDrive from four DcMotors.
+   *
+   * @param motorL1 One of the left DcMotors
+   * @param motorR1 One of the right DcMotors
+   * @param motorL2 The other left DcMotor
+   * @param motorR2 The other right DcMotor
+   * @param opMode The OpMode to set
+   * @param ticksPerInch How many ticks per inch of displacement
+   * @param ticksPer360 How many ticks in one full rotation of the motor
+   *
+   * @return A TankDrive created using MotorPairs
+   */
+  public static TankDrive fromMotors(DcMotor motorL1, DcMotor motorL2, DcMotor motorR1, DcMotor motorR2,
       OpMode opMode, int ticksPerInch, int ticksPer360) {
 
     return new TankDrive(
-        MotorPair.of(left1, left2),
-        MotorPair.of(right1, right2), opMode, ticksPerInch, ticksPer360);
+        MotorPair.of(motorL1, motorL2),
+        MotorPair.of(motorR1, motorR2), opMode, ticksPerInch, ticksPer360);
   }
 
-
+  /**
+   * Drive forward a certain amount with encoders.
+   *
+   * @param distanceInInches The distance to move forwards
+   * @param power The motor power
+   */
   @Override
   public void driveForwards(double distanceInInches, double power) {
     driveWithEncoder(Math.abs(distanceInInches), Math.abs(power));
@@ -68,11 +107,23 @@ public class TankDrive extends DriveTrain {
     runWithEncoder((int) robotTurn, (int) robotTurn, power, power);
   }
 
+  /**
+   * Drive backwards a certain amount with encoders.
+   *
+   * @param distanceInInches The distance to move backwards
+   * @param power The motor power
+   */
   @Override
   public void driveBackwards(double distanceInInches, double power) {
     driveWithEncoder(-Math.abs(distanceInInches), -Math.abs(power));
   }
 
+  /**
+   * Rotate a certain amount with encoders clockwise.
+   *
+   * @param degrees The desired clockwise rotation in degrees
+   * @param givenPower The motor power
+   */
   @Override
   public void rotateClockwise(int degrees, double givenPower) {
     double power = Range.clip(givenPower, -1, 1);
@@ -82,6 +133,12 @@ public class TankDrive extends DriveTrain {
     rotateWithEncoder(normalizedDegrees, -normalizedDegrees, power, -power);
   }
 
+  /**
+   * Rotate a certain amount with encoders counterclockwise.
+   *
+   * @param degrees The desired counterclockwise rotation in degrees
+   * @param givenPower The motor power
+   */
   @Override
   public void rotateCounterClockwise(int degrees, double givenPower) {
     double power = Range.clip(givenPower, -1, 1);
@@ -120,6 +177,11 @@ public class TankDrive extends DriveTrain {
 
   // -- TeleOp methods --
 
+  /**
+   * Set the power of the motors to move in a straight line without encoders.
+   *
+   * @param power The motor power to set [-1, 1]
+   */
   @Override
   public void setMovementPower(double power) {
     setMotorMode(RUN_WITHOUT_ENCODER);
@@ -128,6 +190,11 @@ public class TankDrive extends DriveTrain {
     motorR.setPower(power);
   }
 
+  /**
+   * Set the power of the motors to rotate without encoders.
+   *
+   * @param power The motor power to set [-1, 1]
+   */
   @Override
   public void setRotationPower(double power) { //clockwise if power is positive
     setMotorMode(RUN_WITHOUT_ENCODER);
@@ -136,6 +203,12 @@ public class TankDrive extends DriveTrain {
     motorR.setPower(-power);
   }
 
+  /**
+   * Set both the movement power and the rotation power of the the motors.
+   *
+   * @param movePower The motor power to set for movement [-1, 1]
+   * @param rotatePower The motor power to set for rotation [-1, 1]
+   */
   @Override
   public void setMovementAndRotation(double movePower, double rotatePower) {
     double leftPower = movePower + rotatePower,
