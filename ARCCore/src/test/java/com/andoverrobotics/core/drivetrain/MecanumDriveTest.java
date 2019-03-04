1c4d44aaeec7fa646f5baf56299e50a55e5a8f7c
Michael <michael@mcmoo.org>
2019-03-04 17:35:54 -0500

    Isolate individual motors instead of pairs in MecanumDrive

--- ARCCore/src/test/java/com/andoverrobotics/core/drivetrain/MecanumDriveTest.java
+++ ARCCore/src/test/java/com/andoverrobotics/core/drivetrain/MecanumDriveTest.java
@@ -9,28 +9,41 @@ import static org.mockito.Mockito.verifyNoMoreInteractions;
 
 import com.andoverrobotics.core.utilities.Coordinate;
 import com.andoverrobotics.core.utilities.IMotor;
+import com.andoverrobotics.core.utilities.MotorAdapter;
 import com.andoverrobotics.core.utilities.MotorPair;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotor.RunMode;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.AdditionalMatchers;
 
 public class MecanumDriveTest {
-  private OpMode opMode = mock(OpMode.class);
-  private MotorPair
-      rightDiagonal = mock(MotorPair.class),
-      leftDiagonal = mock(MotorPair.class),
-      rightSide = mock(MotorPair.class),
-      leftSide = mock(MotorPair.class);
 
-  private StrafingDriveTrain driveTrain = new MecanumDrive(
-      leftDiagonal, rightDiagonal, leftSide, rightSide,
-      opMode, 5, 100);
+  private OpMode opMode = mock(OpMode.class);
+  private MotorAdapter motorFL = mock(MotorAdapter.class),
+      motorFR = mock(MotorAdapter.class),
+      motorBL = mock(MotorAdapter.class),
+      motorBR = mock(MotorAdapter.class);
+  // This is an octagonal config
+  private MotorAdapter[] leftSide = {motorFL, motorBL},
+      rightSide = {motorFR, motorBR},
+      leftDiagonal = {motorBL, motorFR},
+      rightDiagonal = {motorBR, motorFL},
+      allMotors = {motorFL, motorFR, motorBL, motorBR};
+
+  private StrafingDriveTrain driveTrain = new MecanumDrive(motorFL, motorFR, motorBL, motorBR,
+      new MotorAdapter[]{
+          motorFR,
+          motorBL
+      }, new MotorAdapter[]{
+      motorFL,
+      motorBR
+  }, opMode, 5, 100);
 
   @Before
   public void setUp() {
-    reset(rightDiagonal, leftDiagonal, rightSide, leftSide, opMode);
+    reset(motorFL, motorFR, motorBL, motorBR, opMode);
   }
 
   @Test
@@ -39,8 +52,7 @@ public class MecanumDriveTest {
 
     int tickOffset = (int) (5 * 5 / Math.sqrt(2));
 
-    verifyRunToPosition(leftDiagonal, tickOffset, 1);
-    verifyRunToPosition(rightDiagonal, tickOffset, 1);
+    verifyAllMotorsRunToPosition(tickOffset, 1);
   }
 
   @Test
@@ -49,8 +61,7 @@ public class MecanumDriveTest {
 
     int tickOffset = (int) (-8 * 5 / Math.sqrt(2));
 
-    verifyRunToPosition(leftDiagonal, tickOffset, -0.6);
-    verifyRunToPosition(rightDiagonal, tickOffset, -0.6);
+    verifyAllMotorsRunToPosition(tickOffset, 0.6);
   }
 
   @Test
@@ -59,8 +70,7 @@ public class MecanumDriveTest {
 
     int tickOffset = (int) (-8 * 5 / Math.sqrt(2));
 
-    verifyRunToPosition(leftDiagonal, tickOffset, -0.4);
-    verifyRunToPosition(rightDiagonal, tickOffset, -0.4);
+    verifyAllMotorsRunToPosition(tickOffset, 0.4);
   }
 
   @Test
@@ -69,40 +79,39 @@ public class MecanumDriveTest {
 
     int tickOffset = (int) (-4 * 5 / Math.sqrt(2));
 
-    verifyRunToPosition(leftDiagonal, tickOffset, -0.6);
-    verifyRunToPosition(rightDiagonal, tickOffset, -0.6);
+    verifyAllMotorsRunToPosition(tickOffset, 0.6);
   }
 
   @Test
   public void rotateClockwise() {
     driveTrain.rotateClockwise(50, 0.6);
 
-    verifyRunToPosition(leftSide, (int) (50 / 360.0 * 100), 0.6);
-    verifyRunToPosition(rightSide, (int) (-50 / 360.0 * 100), 0.6);
+    verifyRunToPosition((int) (50 / 360.0 * 100), 0.6, motorBL, motorFL);
+    verifyRunToPosition((int) (-50 / 360.0 * 100), 0.6, motorBR, motorFR);
   }
 
   @Test
   public void rotateClockwiseNegativeDegree() {
     driveTrain.rotateClockwise(-36, 0.2);
 
-    verifyRunToPosition(leftSide, (int) ((360 - 36) / 360.0 * 100), 0.2);
-    verifyRunToPosition(rightSide, (int) ((36 - 360) / 360.0 * 100), 0.2);
+    verifyRunToPosition((int) ((360 - 36) / 360.0 * 100), 0.2, leftSide);
+    verifyRunToPosition((int) ((36 - 360) / 360.0 * 100), 0.2, rightSide);
   }
 
   @Test
   public void rotateCounterClockwise() {
     driveTrain.rotateCounterClockwise(50, -0.5);
 
-    verifyRunToPosition(leftSide, (int) (-50 / 360.0 * 100), 0.5);
-    verifyRunToPosition(rightSide, (int) (50 / 360.0 * 100), 0.5);
+    verifyRunToPosition((int) (-50 / 360.0 * 100), 0.5, leftSide);
+    verifyRunToPosition((int) (50 / 360.0 * 100), 0.5, rightSide);
   }
 
   @Test
   public void rotateCounterClockwiseNegativeDegree() {
     driveTrain.rotateCounterClockwise(-70, -0.8);
 
-    verifyRunToPosition(leftSide, (int) ((70 - 360) / 360.0 * 100), 0.8);
-    verifyRunToPosition(rightSide, (int) ((360 - 70) / 360.0 * 100), 0.8);
+    verifyRunToPosition((int) ((70 - 360) / 360.0 * 100), 0.8, leftSide);
+    verifyRunToPosition((int) ((360 - 70) / 360.0 * 100), 0.8, rightSide);
   }
 
   @Test
@@ -111,8 +120,8 @@ public class MecanumDriveTest {
 
     int tickOffset = (int) (20.5 * 5 / Math.sqrt(2));
 
-    verifyRunToPosition(leftDiagonal, -tickOffset, -0.6);
-    verifyRunToPosition(rightDiagonal, tickOffset, 0.6);
+    verifyRunToPosition(-tickOffset, -0.6, leftDiagonal);
+    verifyRunToPosition(tickOffset, 0.6, rightDiagonal);
   }
 
   @Test
@@ -121,8 +130,8 @@ public class MecanumDriveTest {
 
     int tickOffset = (int) (14.8 * 5 / Math.sqrt(2));
 
-    verifyRunToPosition(leftDiagonal, -tickOffset, -0.7);
-    verifyRunToPosition(rightDiagonal, tickOffset, 0.7);
+    verifyRunToPosition(-tickOffset, -0.7, leftDiagonal);
+    verifyRunToPosition(tickOffset, 0.7, rightDiagonal);
   }
 
   @Test
@@ -131,8 +140,8 @@ public class MecanumDriveTest {
 
     int tickOffset = (int) (23.1 * 5 / Math.sqrt(2));
 
-    verifyRunToPosition(leftDiagonal, tickOffset, 0.4);
-    verifyRunToPosition(rightDiagonal, -tickOffset, -0.4);
+    verifyRunToPosition(tickOffset, 0.4, leftDiagonal);
+    verifyRunToPosition(-tickOffset, -0.4, rightDiagonal);
   }
 
   @Test
@@ -141,23 +150,23 @@ public class MecanumDriveTest {
 
     int tickOffset = (int) (24.5 * 5 / Math.sqrt(2));
 
-    verifyRunToPosition(leftDiagonal, tickOffset, 0.64);
-    verifyRunToPosition(rightDiagonal, -tickOffset, -0.64);
+    verifyRunToPosition(tickOffset, 0.64, leftDiagonal);
+    verifyRunToPosition(-tickOffset, -0.64, rightDiagonal);
   }
 
   @Test
   public void strafeInches() {
     driveTrain.strafeInches(10, 15, 0.4);
 
-    verifyRunToPosition(leftDiagonal, (int) (3.5355339 * 5), 0.08);
-    verifyRunToPosition(rightDiagonal, (int) (17.67766953 * 5), 0.4);
+    verifyRunToPosition((int) (3.5355339 * 5), 0.08, leftDiagonal);
+    verifyRunToPosition((int) (17.67766953 * 5), 0.4, rightDiagonal);
   }
 
   @Test
   public void strafeInchesNullVector() {
     driveTrain.strafeInches(0, 0, 0.4);
 
-    verifyNoMoreInteractions(leftDiagonal, rightDiagonal);
+    verifyNoMoreInteractions((Object[]) allMotors);
   }
 
   @Test
@@ -200,41 +209,49 @@ public class MecanumDriveTest {
   public void setStrafeWithinUnitCircle() {
     driveTrain.setStrafe(Coordinate.fromXY(0.5, 0.7), 1);
 
-    verifyPairPower(leftDiagonal, 0.141421356);
-    verifyPairPower(rightDiagonal, 0.8485281374);
+    verifyPairPower(0.141421356, leftDiagonal);
+    verifyPairPower(0.8485281374, rightDiagonal);
   }
 
   @Test
   public void setStrafeOutsideUnitCircle() {
     driveTrain.setStrafe(Coordinate.fromXY(525, -1441), 0.5);
 
-    verifyPairPower(leftDiagonal, -0.45322105262);
-    verifyPairPower(rightDiagonal, -0.211165047915);
+    verifyPairPower(-0.45322105262, leftDiagonal);
+    verifyPairPower(-0.211165047915, rightDiagonal);
   }
 
   @Test
   public void setStrafeWithOrigin() {
     driveTrain.setStrafe(0, 0, Double.MAX_VALUE);
 
-    verifyPairPower(leftDiagonal, 0);
-    verifyPairPower(rightDiagonal, 0);
+    verifyPairPower(0, leftDiagonal);
+    verifyPairPower(0, rightDiagonal);
   }
 
   private void verifyPowersWithoutEncoder(double leftPower, double rightPower) {
-    verify(leftSide).setMode(RunMode.RUN_WITHOUT_ENCODER);
-    verify(rightSide).setMode(RunMode.RUN_WITHOUT_ENCODER);
 
-    verifyPairPower(leftSide, leftPower);
-    verifyPairPower(rightSide, rightPower);
+    for (IMotor motor : allMotors) {
+      verify(motor).setMode(RunMode.RUN_WITHOUT_ENCODER);
+    }
+
+    verifyPairPower(leftPower, leftSide);
+    verifyPairPower(rightPower, rightSide);
   }
 
-  private void verifyRunToPosition(IMotor mock, int tickOffset, double power) {
-    verify(mock).startRunToPosition(eq(tickOffset),
-        or(AdditionalMatchers.eq(power, 1e-4),
-            AdditionalMatchers.eq(-power, 1e-4)));
+  private void verifyAllMotorsRunToPosition(int tickOffset, double power) {
+    verifyRunToPosition(tickOffset, power, motorFL, motorFR, motorBL, motorBR);
   }
 
-  private void verifyPairPower(MotorPair pair, double power) {
-    verify(pair).setPower(AdditionalMatchers.eq(power, 1e-3));
+  private void verifyRunToPosition(int tickOffset, double power, MotorAdapter... mocks) {
+    for (MotorAdapter mock : mocks) {
+      verify(mock).startRunToPosition(eq(tickOffset), AdditionalMatchers.eq(Math.abs(power), 1e-4));
+    }
+  }
+
+  private void verifyPairPower(double power, MotorAdapter... pair) {
+    for (MotorAdapter motor : pair) {
+      verify(motor).setPower(AdditionalMatchers.eq(power, 1e-3));
+    }
   }
 }
